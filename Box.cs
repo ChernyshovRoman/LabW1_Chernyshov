76d65b733c77d3eab69fa0d292a0f2f26e3a7021
Forgisih <ivankalmykov836@gmail.com>
2025-01-16 12:04:14 +0300

    Добавьте файлы проекта.

---
+++ Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW1_Chernyshov
+{
+    internal class Box
+    {
+        public float height { get; set; }
+        public float width { get; set; }
+        public float length { get; set; }
+        public Box()
+        {
+            height = 0;
+            width = 0;
+            length = 0;
+        }
+        public Box(float h, float w, float l)
+        {
+            height = h;
+            width = w;
+            length = l;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Данная коробка имеет ширину в {width} см, высоту в {height} см и длину в {length} см");
+        }
+        public float Volume()
+        {
+            float Vol = height * width * length;
+            return Vol;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW1_Chernyshov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box crate = new Box();
+            crate.width = 2;
+            crate.height = 3;
+            crate.length = 5;
+            crate.Show();
+            Console.WriteLine($"Объём коробки равен {crate.Volume()} см^2");
+            Console.ReadLine();
+        }
+    }
+}
